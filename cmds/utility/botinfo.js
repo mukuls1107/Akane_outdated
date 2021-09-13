95a350ac62a0e48b237b71b567666bcab4f11da2
damon00-dev <80405570+damon00-dev@users.noreply.github.com>
2021-09-13 15:24:57 +0530

    Update botinfo.js

--- cmds/utility/botinfo.js
+++ cmds/utility/botinfo.js
@@ -5,6 +5,11 @@ const ms = require("ms");
 
 module.exports.run = async (bot, message, args) => {
   message.delete();
+   let bicon = bot.user.displayAvatarURL;
+    let usersize = bot.users.size
+    let chansize = bot.channels.size
+    let uptimxd = bot.uptime 
+    let servsize = bot.guilds.size
   let botembed = new Discord.RichEmbed()
   .setTitle("Bot Info")
   .setDescription(`This bot was created for the DevHub Server of Discord to have fun.`)
@@ -12,7 +17,10 @@ module.exports.run = async (bot, message, args) => {
   .setColor("GREEN")
   .addField(`- Bot creators: \n`, `> DAMOÑ#8497\n> <@587517896133967884>\n\n> @Akshu✨#6552\n> <@585711075413065728>`)
   .addField(`- Bot info: \n`, `> ${bot.user.tag}\n> 870174977914196058`)
-  .addField(`- Created on: `, `> ${(bot.createdAt).format("dddd, MMMM Do YYYY")}`)
+  .addField(`- Created on: `, `> ${(bot.createdAt)("dddd, MMMM Do YYYY")}`)
+  .addField(`- Servers: `, `🛡 ${servsize}`)
+    .addField(`- channels: `, `📁 ${chansize}`)
+    .addField(`- Users: `, `🙆‍♂️ ${usersize}` )
   
   message.channel.send(botembed).then(m => m.delete(5000));
 }
