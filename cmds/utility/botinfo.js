6a1d7b327a1fce6eb3739c6a4a9a7ca70a8e38e5
damon00-dev <80405570+damon00-dev@users.noreply.github.com>
2021-09-13 15:04:55 +0530

    Update botinfo.js

--- cmds/utility/botinfo.js
+++ cmds/utility/botinfo.js
@@ -12,7 +12,7 @@ module.exports.run = async (bot, message, args) => {
   .setColor("GREEN")
   .addField(`- Bot creators: \n`, `> DAMOÑ#8497\n> <@587517896133967884>\n\n> @Akshu✨#6552\n> <@585711075413065728>`)
   .addField(`- Bot info: \n`, `> ${bot.user.tag}\n> 870174977914196058`)
-  .addField(`- Created on: `, `> ${moment.utc(bot.user.createdAt).format("dddd, MMMM Do YYYY")} (${ms(Date.now()- bot.user.createdAt, {long: true})})`)
+  .addField(`- Created on: `, `> ${(bot.user.createdAt).format("dddd, MMMM Do YYYY")}`)
   
   message.channel.send(botembed).then(m => m.delete(5000));
 }
